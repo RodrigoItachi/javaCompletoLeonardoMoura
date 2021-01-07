3bc68f18a44059bb6689aab97c4052c47053ffed
RodrigoItachi <sousarodrigo56@outlook.com>
2021-01-07 15:15:21 -0300

    Foram criados a Classe Aluno e Curso

---
+++ src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/manytomany/Curso.java
@@ -0,0 +1,8 @@
+package javaCompletoLeonardoMoura.br.com.poo.classe.composicao.manytomany;
+
+import java.util.ArrayList;
+
+public class Curso {
+	String nome;
+	ArrayList<Aluno> alunos = new ArrayList<>();
+}

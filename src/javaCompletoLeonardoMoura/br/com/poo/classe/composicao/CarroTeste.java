78c4788aca2dcc838f747474539e653b0a4dadd4
RodrigoItachi <sousarodrigo56@outlook.com>
2021-01-06 23:18:48 -0300

    Foi add uma instância do tipo Carro

--- src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/CarroTeste.java
+++ src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/CarroTeste.java
@@ -3,8 +3,7 @@ package javaCompletoLeonardoMoura.br.com.poo.classe.composicao;
 public class CarroTeste {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Carro carro = new Carro();
 	}
 
 }

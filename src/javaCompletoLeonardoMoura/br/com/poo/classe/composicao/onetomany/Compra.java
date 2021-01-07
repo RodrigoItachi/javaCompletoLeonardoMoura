2b37dec76c87d31630dbe15544a3d7aa3cb63d88
RodrigoItachi <sousarodrigo56@outlook.com>
2021-01-07 12:45:14 -0300

    subindo novas classes com novos pacotes

---
+++ src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/onetomany/Compra.java
@@ -0,0 +1,11 @@
+package javaCompletoLeonardoMoura.br.com.poo.classe.composicao.onetomany;
+
+import java.util.ArrayList;
+
+public class Compra {
+	String nomeCliente;
+	ArrayList<Produto> produtos = new ArrayList<>();
+	
+	
+	
+}

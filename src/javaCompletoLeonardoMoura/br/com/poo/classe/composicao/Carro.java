f3878480f86bc4b390b41ad901fc951040f00483
RodrigoItachi <sousarodrigo56@outlook.com>
2021-01-06 23:53:56 -0300

    foram adicionados intrunções para evitar que o RPM passasse do necessário tanto para cima como para baixo

--- src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/Carro.java
+++ src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/Carro.java
@@ -4,21 +4,25 @@ public class Carro {
 	Motor motor = new Motor();
 
 	void acelerar() {
-		motor.fatorInjecao += 0.4;
+		if (motor.fatorInjecao < 2.6) {
+			motor.fatorInjecao += 0.4;
+		}
 	}
-	
+
 	void frear() {
-		motor.fatorInjecao -= 0.4;
+		if (motor.fatorInjecao < 0.5) {
+			motor.fatorInjecao -= 0.4;
+		}
 	}
-	
+
 	void motorLigado() {
 		motor.ligado = true;
 	}
-	
+
 	void motorDeligar() {
 		motor.ligado = false;
 	}
-	
+
 	boolean estaLigado() {
 		return motor.ligado;
 	}

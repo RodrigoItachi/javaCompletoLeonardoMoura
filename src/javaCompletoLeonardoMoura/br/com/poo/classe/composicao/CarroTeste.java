ddaab1552f1aab95f520b6c403724a8c452a5bdc
RodrigoItachi <sousarodrigo56@outlook.com>
2021-01-06 23:10:45 -0300

    Subindo a classe CarroTeste com o método main

---
+++ src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/CarroTeste.java
@@ -0,0 +1,10 @@
+package javaCompletoLeonardoMoura.br.com.poo.classe.composicao;
+
+public class CarroTeste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}

cccc0cea0104b2208a24e42aef81fb7fb17e4d5e
RodrigoItachi <sousarodrigo56@outlook.com>
2021-01-06 23:24:51 -0300

    foram add três linhas para verificar se o motor está ligado

--- src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/CarroTeste.java
+++ src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/CarroTeste.java
@@ -4,6 +4,10 @@ public class CarroTeste {
 
 	public static void main(String[] args) {
 		Carro carro = new Carro();
+
+		System.out.println(carro.estaLigado());
+		carro.motorLigado();
+		System.out.println(carro.estaLigado());
 	}
 
 }

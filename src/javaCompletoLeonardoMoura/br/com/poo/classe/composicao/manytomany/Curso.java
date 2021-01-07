6585361be690bb2ee28bce4c3edfe1e45e1ce22a
RodrigoItachi <sousarodrigo56@outlook.com>
2021-01-07 16:45:55 -0300

    foi add um método addAluno na Classe Curso

--- src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/manytomany/Curso.java
+++ src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/manytomany/Curso.java
@@ -5,4 +5,9 @@ import java.util.ArrayList;
 public class Curso {
 	String nome;
 	ArrayList<Aluno> alunos = new ArrayList<>();
+
+	void addAluno(Aluno aluno) {
+		this.alunos.add(aluno);
+		aluno.cursos.add(this);
+	}
 }

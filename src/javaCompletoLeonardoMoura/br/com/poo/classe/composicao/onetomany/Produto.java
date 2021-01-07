a127c4f253828a591b90d07f0c2d555cedf4fcf5
RodrigoItachi <sousarodrigo56@outlook.com>
2021-01-07 13:55:09 -0300

    Foram add dois construtores um vazio e um com todos os atributos

--- src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/onetomany/Produto.java
+++ src/javaCompletoLeonardoMoura/br/com/poo/classe/composicao/onetomany/Produto.java
@@ -1,9 +1,18 @@
 package javaCompletoLeonardoMoura.br.com.poo.classe.composicao.onetomany;
 
 public class Produto {
-	
+
 	String nome;
 	int quantidade;
 	double preco;
-	
+
+	public Produto() {
+	}
+
+	public Produto(String nome, int quantidade, double preco) {
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.preco = preco;
+	}
+
 }
